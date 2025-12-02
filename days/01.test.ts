41d74b7297475377acf186f95262e79e36cad818
DELTA-N\akasha <akasha@delta-n.nl>
2025-12-02 20:24:12 +0100

    一日第一問

--- days/01.test.ts
+++ days/01.test.ts
@@ -2,11 +2,20 @@ import {assertEquals} from "std/assert/assert_equals.ts";
 import '../extension-methods.ts';
 import {p1, p2} from './01.ts';
 
-const input = ``;
+const input = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`;
 
 Deno.test('it should run the first part of day 01 correctly', () => {
   const result = p1(input);
-  const expected = 0;
+  const expected = 3;
   assertEquals(result, expected);
 });
 
--- days/01.ts
+++ days/01.ts
@@ -1,11 +1,55 @@
 import '../extension-methods.ts';
 
-export const p1 = (input: string): number => {
-  const lines = input.splitRows().map(Number);
-  return 0 //lines.length;
+type 數=number;
+type 文=string;
+type 文列=文[];
+type 空=void;
+var 文作 = String
+
+//第一問：
+export const p1 = (入文: 文): 數 => {
+  const 行列: 文列 = 入文.splitRows().map(文作);
+  let 零計 = 0;
+  let 此位 = 50;
+  行列.forEach(文 => {
+    曰(文);
+    const 方: 文 = 取字(文, 0);
+    const 度:數 = 文成數(取後(文, 1));
+    if (方 === 'R') { // 右轉
+      此位 += 度;
+    } else if (方 === 'L') { // 左轉
+      此位 -= 度;
+    }
+    此位 = 餘環(此位, 100);
+    if (此位 === 0) {
+      零計++;
+    }
+  })
+  return 零計;
 }
 
-export const p2 = (input: string): number => {
-  const lines = input.splitRows().map(Number);
-  return 0 //lines.length;
+//第二問：
+export const p2 = (入文: 文): 數 => {
+  const 行列 = 入文.splitRows().map(文作);
+  return 0;
+}
+
+export const 餘環 = (數: 數, 限: 數): 數 => {
+  return ((數 % 限) + 限) % 限;
+}
+
+export const 曰 = (文: 文): 空 => {
+  console.log(文);
+}
+
+export const 文成數 = (文: 文): 數 => {
+  return parseInt(文, 10);
+}
+
+export const 取字 = (文: 文, 位: 數): 文 => {
+  return 文.charAt(位);
+}
+
+export const 取後 = (文: 文, 位: 數): 文 => {
+  return 文.slice(位);
 }
